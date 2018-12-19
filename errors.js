45410b02005ff4ac30e40996a0b5717815ce1026
Julien Klepatch <julien@julienklepatch.com>
2018-12-19 16:18:57 +0800

    Refactor error handling

--- errors.js
+++ errors.js
@@ -1,3 +1,12 @@
 class NotFound extends Error {}
 
-module.exports = { NotFound };
+const errorHandler = (res, e) => {
+  if(e instanceof NotFound) {
+    res.status(404);
+    return res.send({error: e.message});
+  }
+  res.status(500);
+  res.send({error: 'Service unaivalable'});
+};
+
+module.exports = { NotFound, errorHandler };
--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const config = require('./config');
 const services = require('./services');
-const fs = require('fs');
 const errors = require('./errors');
 
 /**
@@ -11,9 +10,8 @@ router.get('/api/v1/metadata', async (req, res) => {
   try {
     const metadata = await services.getMetadataList();
     res.send(metadata);
-  } catch(_e) {
-    res.status(500);
-    res.send({error: 'Service unaivalable'});
+  } catch(e) {
+    errors.errorHandler(res, e);
   }
 });
 
@@ -26,12 +24,7 @@ router.get('/api/v1/metadata/:id', async (req, res) => {
     const metadata = await services.getMetadata(id);
     res.send(metadata);
   } catch(e) {
-    if(e instanceof errors.NotFound) {
-      res.status(404);
-      return res.send({error: e.message});
-    }
-    res.status(500);
-    res.send({error: 'Service unaivalable'});
+    errors.errorHandler(res, e);
   }
 });
 
@@ -44,12 +37,7 @@ router.get('/api/v1/songs/:id', async (req, res) => {
     const song = await services.getSong(id);
     song.pipe(res);
   } catch(e) {
-    if(e instanceof errors.NotFound) {
-      res.status(404);
-      return res.send({error: e.message});
-    }
-    res.status(500);
-    res.send({error: 'Service unaivalable'});
+    errors.errorHandler(res, e);
   }
 });
 

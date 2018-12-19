e9c36b794e973075b1c22b43aaf8d391cf8ae850
Julien Klepatch <julien@julienklepatch.com>
2018-12-19 16:14:37 +0800

    Refactor routes with services

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const config = require('./config');
 const services = require('./services');
 const fs = require('fs');
+const errors = require('./errors');
 
 /**
  * List of all song metadata
@@ -22,14 +23,13 @@ router.get('/api/v1/metadata', async (req, res) => {
 router.get('/api/v1/metadata/:id', async (req, res) => {
   const id = req.params.id;
   try {
-    const metadata = await services.getMetadataList();
-    const d = metadata.filter((d) => d.id == id);
-    if(d.length == 0) {
+    const metadata = await services.getMetadata(id);
+    res.send(metadata);
+  } catch(e) {
+    if(e instanceof errors.NotFound) {
       res.status(404);
-      return res.send({error: `Metadata with id ${id} was not found`});
+      return res.send({error: e.message});
     }
-    res.send(d[0]);
-  } catch(_e) {
     res.status(500);
     res.send({error: 'Service unaivalable'});
   }
@@ -41,15 +41,13 @@ router.get('/api/v1/metadata/:id', async (req, res) => {
 router.get('/api/v1/songs/:id', async (req, res) => {
   const id = req.params.id;
   try {
-    const metadata = await services.getMetadataList();
-    const d = metadata.filter((d) => d.id == id);
-    if(d.length == 0) {
-      res.status(404);
-      return res.send({error: `Song with id ${id} was not found`});
-    }
-    const song = fs.createReadStream(`${config.SONG_DIR}/${d[0].fileRef}`);
+    const song = await services.getSong(id);
     song.pipe(res);
-  } catch(_e) {
+  } catch(e) {
+    if(e instanceof errors.NotFound) {
+      res.status(404);
+      return res.send({error: e.message});
+    }
     res.status(500);
     res.send({error: 'Service unaivalable'});
   }
--- services.js
+++ services.js
@@ -1,9 +1,29 @@
+const fs = require('fs');
 const utils = require('./utils');
 const config = require('./config');
+const errors = require('./errors');
 
-const getMetadataList = async (args) => {
+const getMetadataList = async() => {
   const metadata = await utils.readFile(config.METADATA);
   return JSON.parse(metadata);
 };
 
-module.exports = { getMetadataList };
+const getMetadata = async(id) => {
+  const metadata = await getMetadataList();
+  const d = metadata.filter((d) => d.id == id);
+  if(d.length == 0) {
+    throw new errors.NotFound(`Song with id ${id} was not found`);
+  }
+  return d[0];
+};
+
+const getSong = async(id) => {
+  const metadata = await getMetadata(id);
+  return fs.createReadStream(`${config.SONG_DIR}/${metadata.fileRef}`);
+};
+
+module.exports = { 
+  getMetadataList,
+  getMetadata,
+  getSong
+};

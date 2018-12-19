2cb98ef53917ad7360f2e54d8210b5f65aee87ac
Julien Klepatch <julien@julienklepatch.com>
2018-12-19 15:51:21 +0800

    Refactor routes with async fileRead

--- routes.js
+++ routes.js
@@ -1,62 +1,63 @@
 const fs = require('fs');
 const router = require('express').Router();
 const config = require('./config');
+const utils = require('./utils');
 
 /**
  * List of all song metadata
  */
-router.get('/api/v1/metadata', (req, res) => {
-  fs.readFile(config.METADATA, 'utf8', (err, data) => {
-    if(err) {
-      res.status(500);
-      return res.send({error: 'Service unaivalable'});
-    }
-    //data is a string and express set content-type to html
+router.get('/api/v1/metadata', async (req, res) => {
+  try {
+    const metadata = await utils.readFile(config.METADATA);
+    //metadata is a string and express set content-type to html
     //for strings. Forces express to respond as json instead
     res.setHeader('Content-Type', 'application/json');
-    res.send(data);
-  });
+    res.send(metadata);
+  } catch(_e) {
+    res.status(500);
+    res.send({error: 'Service unaivalable'});
+  }
 });
 
 /**
  * Single song metadata
  */
-router.get('/api/v1/metadata/:id', (req, res) => {
+router.get('/api/v1/metadata/:id', async (req, res) => {
   const id = req.params.id;
-  fs.readFile(config.METADATA, 'utf8', (err, data) => {
-    if(err) {
-      res.status(500);
-      return res.send({error: 'Service unaivalable'});
-    }
-    data = JSON.parse(data);
-    const d = data.filter((d) => d.id == id);
+  try {
+    let metadata = await utils.readFile(config.METADATA);
+    metadata = JSON.parse(metadata);
+    const d = metadata.filter((d) => d.id == id);
     if(d.length == 0) {
       res.status(404);
       return res.send({error: `Metadata with id ${id} was not found`});
     }
     res.send(d[0]);
-  });
+  } catch(_e) {
+    res.status(500);
+    res.send({error: 'Service unaivalable'});
+  }
 });
 
 /**
  * Single song file
  */
-router.get('/api/v1/songs/:id', (req, res) => {
+router.get('/api/v1/songs/:id', async (req, res) => {
   const id = req.params.id;
-  fs.readFile(config.METADATA, 'utf8', (err, data) => {
-    if(err) {
-      res.status(500);
-      return res.send({error: 'Service unaivalable'});
-    }
-    data = JSON.parse(data);
-    const d = data.filter((d) => d.id == id);
+  try {
+    let metadata = await utils.readFile(config.METADATA);
+    metadata = JSON.parse(metadata);
+    const d = metadata.filter((d) => d.id == id);
     if(d.length == 0) {
       res.status(404);
       return res.send({error: `Song with id ${id} was not found`});
     }
     const song = fs.createReadStream(`${config.SONG_DIR}/${d[0].fileRef}`);
     song.pipe(res);
-  });
+  } catch(_e) {
+    res.status(500);
+    res.send({error: 'Service unaivalable'});
+  }
 });
 
 module.exports = router;
---
+++ utils.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const readFile = (path, opts = 'utf8') => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, opts, (err, data) => {
+      if(err) {
+        return reject(err);
+      }
+      resolve(data)
+    });
+  });
+};
+
+module.exports = { readFile };

43497d3bc2efc95ebb777712f6b49f34bb159fd6
Julien Klepatch <julien@julienklepatch.com>
2019-01-08 22:12:27 +0800

    Fix eslint

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import configureStore from './store';
+//eslint-disable-next-line no-unused-vars
 import css from './scss/index.scss';
 
 render(
